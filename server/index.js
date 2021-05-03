4675bebf939424f53a03cd06a2d081fea4e97dc5
parthmangrola <parthmangrola@gmail.com>
2021-05-02 22:27:04 -0700

    :construction: Added Passport and JWT for signup

--- server/index.js
+++ server/index.js
@@ -4,18 +4,33 @@ const passport = require("passport");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+require("./models/User");
 require("./services/passport");
 
-mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.set("useCreateIndex", true);
+
+mongoose.connection.on("error", (error) => console.log(error));
+
+const authRoute = require("./routes/authRoutes");
+const secureRoute = require("./routes/secureRoutes");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(authRoute);
 
-app.get("/", (req, res) => res.send("Hello world!"));
+// Plug in the JWT strategy as a middleware so only verified users can access this route.
+app.use("/user", passport.authenticate("jwt", { session: false }), secureRoute);
 
+// Handle errors.
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
 const port = process.env.PORT || 8000;
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
